6fd329c56cf7a2d3ef227dcdaa9bcf2d3e038d6d
Ghassen742 <kerfaigassen@gmail.com>
2026-05-07 10:59:51 +0100

    add CORS support

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mysql = require("mysql2/promise");
 
 const app = express();
+const cors = require("cors");
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
